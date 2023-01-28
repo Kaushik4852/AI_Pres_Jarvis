93d8405bc7946161752b28727586815404801166
Kaushik Rathva <79138524+Kaushik4852@users.noreply.github.com>
2023-01-28 04:51:12 +0000

    update

--- AI_pres.py
+++ AI_pres.py
@@ -1,44 +1,40 @@
 import sys
 init_game_board = [['','',''],['','',''],['','','']]
-ai = 'O'
-human = 'X'
-tie = 'tie'
+AI = 'O'
+HUMAN = 'X'
+TIE = 'tie'
 values = {'X':1,'O':-1,'tie':0}
 
-
-# def driver(init):
-#     print(game_over(game_board))
-    
 def if_game_over(game_board):
     #Diagonal values check
-    if(game_board[0][0] == game_board[1][1] == game_board[2][2] == human):
-        return values[human];
-    elif(game_board[0][0] == game_board[1][1] == game_board[2][2] == ai):
-        return values[ai];
+    if(game_board[0][0] == game_board[1][1] == game_board[2][2] == HUMAN):
+        return values[HUMAN]
+    elif(game_board[0][0] == game_board[1][1] == game_board[2][2] == AI):
+        return values[AI]
 
     #Check horizontally
     for i in range(0,3):
-        if(game_board[i][0] == game_board[i][1] == game_board[i][2] == human):
-            return values[human];
-        elif(game_board[i][0] == game_board[i][1] == game_board[i][2] == ai):
-            return values[ai];
+        if(game_board[i][0] == game_board[i][1] == game_board[i][2] == HUMAN):
+            return values[HUMAN]
+        elif(game_board[i][0] == game_board[i][1] == game_board[i][2] == AI):
+            return values[AI]
 
     #Check vertically
     for j in range(0,3):
-        if(game_board[0][j] == game_board[1][j] == game_board[2][j] == human):
-            return values[human];
-        elif(game_board[0][j] == game_board[1][j] == game_board[2][j] == ai):
-            return values[ai];
+        if(game_board[0][j] == game_board[1][j] == game_board[2][j] == HUMAN):
+            return values[HUMAN]
+        elif(game_board[0][j] == game_board[1][j] == game_board[2][j] == AI):
+            return values[AI]
 
     #check for tie
-    tie = True
+    t = True
     for i in range(0,3):
         for j in range(0,3):
             if(game_board[i][j]==''):
-                tie=False
+                t=False
     
-    if(tie):
-        return values[tie];
+    if(t):
+        return values[TIE]
     
 def minmax(game_board,isMaximizing,turn):
     if(isMaximizing):
@@ -46,26 +42,20 @@ def minmax(game_board,isMaximizing,turn):
         for i in range(0,3):
             for j in range(0,3):
                 if(game_board[i][j]==''):
-                    game_board[i][j]=turn;
-                    score = if_game_over();
-                    if(score==values[human] or score==values[ai] or score==values[tie]):
-                        return score;
-                    return max(bestScore,minmax(game_board,False,ai))
+                    game_board[i][j]=turn
+                    score = if_game_over(game_board)
+                    if(score==values[HUMAN] or score==values[AI] or score==values[TIE]):
+                        return score
+                    return max(bestScore,minmax(game_board,False,AI))
     else:
         bestScore = sys.maxsize
         for i in range(0,3):
             for j in range(0,3):
                 if(game_board[i][j]==''):
-                    game_board[i][j]=turn;
-                    score = if_game_over();
+                    game_board[i][j]=turn
+                    score = if_game_over(game_board)
                     if(score==1 or score==-1 or score==0):
-                        return score;
-                    return min(bestScore,minmax(game_board,False,human))
+                        return score
+                    return min(bestScore,minmax(game_board,False,HUMAN))
 
-
-    
-
-game_board = [['O','O','O'],
-              ['X','O','O'],
-              ['X','O','X']]
-print(if_game_over(game_board))
+print(minmax(init_game_board, False, AI))
